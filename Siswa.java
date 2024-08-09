16c2e6956679eddc459cffb0a9f96bffde6b7ad6
bima242 <arya_tantular_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 14:18:47 +0700

    Add files via upload

---
+++ Siswa.java
@@ -0,0 +1,16 @@
+// Driver Class
+public class Siswa {
+int id;
+String Nama;
+double ipk;
+
+// Constructor
+// nama harus sama dgn Class
+public Siswa() {
+    id = 0;
+Nama = "kosong";
+ipk = 0.0;
+
+}
+    
+}
